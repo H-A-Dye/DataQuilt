c168325ef430b7e3104295c9ee8220047bd55a54
heatheranndye <32517963+heatheranndye@users.noreply.github.com>
2022-10-06 07:25:10 -0500

    Changes from 10/5

--- getdata2.py
+++ getdata2.py
@@ -1,1 +1,31 @@
 # create a def to download the data from NOAA
+import requests
+
+myurl ='https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&datatypeid=TMIN&datatypeid=TMAX&locationid=ZIP:28801&startdate=2010-05-01&enddate=2010-05-30&limit=366'
+mytoken = "vgMXMUXAOFMvAecQhqyKdZzwBojmbUeb"
+head={"token": mytoken}
+#response = requests.get(myurl, headers=head)
+#response = response.json()
+#theresults=response.get("results")
+mydict = dict()
+
+def get_temps_p(zipco, theyear):
+    # implement with zipcode and year options
+    pass
+
+def get_temps():
+    response= requests.get(myurl,headers=head).json()
+    thedata = response.get('results')
+    return thedata
+
+
+def extract_data(x):
+    # get date, Tmax, Tmin
+    mydate = x.get('date')
+    mytype = x.get('datatype')
+    myvalue = x.get('value')
+    return mydate, mytype, myvalue
+
+
+def place_into_dict():
+    # place
