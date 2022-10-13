b161823124d3d6f5824a3243512e52281e85691f
heatheranndye <32517963+heatheranndye@users.noreply.github.com>
2022-10-13 12:32:37 -0500

    Work from 10/13

--- SetUpWS.py
+++ SetUpWS.py
@@ -1,34 +1,28 @@
-import os
+""" This module selects weather stations that provide TMAX data for the requested year from the inventory of
+GHCND stations. The selected stations are sorted by distance and the top 10 are returned
+reference package 1: https://github.com/gojiplus/get-weather-data/blob/master/zip2ws/zip2ws.pyreference package 2: https://github.com/paulokuong/noaa"""
+import pathlib
 from collections import namedtuple
 import math
-import csv
-import pandas
 import pandas as pd
-import requests
 from urllib.request import urlretrieve
 from geopy.geocoders import Nominatim
 
-# Set up to select weather station
-# reference package 1: https://github.com/gojiplus/get-weather-data/blob/master/zip2ws/zip2ws.py
-# reference package 2: https://github.com/paulokuong/noaa
+PATH = pathlib.Path().absolute()
+LOCALFILE = pathlib.Path().absolute().joinpath('ghcnd-inventory.txt')
 
-# retrieve the list of weather stations
+WeatherStation = 'https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt'
+urlretrieve(WeatherStation,LOCALFILE)
 
-# Get the zip code and convert to latitude and longitude using geocoder
-PATH = os.getcwd()
-Local = os.path.join(PATH,'ws.txt')
-
-WS = 'https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt'
-#urlretrieve(WS,Local)
-
-WSlist=Local
+WeatherStation=LOCALFILE
 
 Station = namedtuple("Station", {'name', 'lat', 'long', 'start','end'})
-# sort the list and reduce to contain only weather stations with TMAX - TMAX List
 
 def filter_ws():
+    """Loads the weather station inventory file and places each station as a named tuple into
+        a list"""
     mywslist=[]
-    with open(Local, encoding='utf8') as t_file:
+    with open(LOCALFILE, encoding='utf8') as t_file:
         mydata = t_file.readlines()
     for x in mydata:
         if x.find('TMAX')!=-1:
@@ -50,30 +44,34 @@ def filter_ws():
     return mywslist
 
 def data_into_df(data):
+    """ load list of stations into a data frame
+    :param data: list of
+    :return: 
+    """
     mydf=pd.DataFrame(data)
     return mydf
 
-WeatherStations = data_into_df(filter_ws())
+WeatherStations_df = data_into_df(filter_ws())
 
-def write_df_to_file(df):
+def write_df_to_file(df , name):
     # write the short (40,0000) to a file?
-    pass
+    df.to_csv(name)
 
 # Get the zip code and convert to latitude and longitude using geocoder
 
 #try1 using pygeocoder - this does not work.
 def zip2latlong(zipcode):
-    # positive lat is north, positive long is east
+    """
+    positive lat is north, positive long is east
+    attempt1: pygeocoder and switched to geopy's nominatim
+    """
     geolocator = Nominatim(user_agent="my_app")
     country = 'USA'
     location = geolocator.geocode(str(zipcode)  + ',' + country)
     return location.latitude, location.longitude
 
-# try2 use https://geopy.readthedocs.io/en/stable/#module-geopy.geocoders
-
-# Compute the distance to each weather station and choose the top 5 or so, return to the GetData.
-
 def convert_latlong(value):
+    """Converts latitude or longitude value to radians"""
     v2, v1 = math.modf(value)
     v2 = v2*100 / 60
     val = v1 +v2
@@ -83,10 +81,10 @@ def convert_latlong(value):
 
 
 def dist_between(lat1, long1, lat2, long2):
-    # d=2*asin(sqrt((sin((lat1-lat2)/2))^2 +
-    #                  cos(lat1)*cos(lat2)*(sin((lon1-lon2)/2))^2))  from edwilliams.org
-    # worked example: http://edwilliams.org/avform147.htm#Example
-    # convert to radians
+    """ d=2*asin(sqrt((sin((lat1-lat2)/2))^2 +
+                      cos(lat1)*cos(lat2)*(sin((lon1-lon2)/2))^2))  from edwilliams.org
+     worked example: http://edwilliams.org/avform147.htm#Example
+    convert to radians """
     rho1 = convert_latlong(lat1)
     rho2 = convert_latlong(lat2)
     lam1 = convert_latlong(long1)
@@ -103,32 +101,29 @@ mylong=-89.92
 laxlat =33.57
 laxlong = 118.24
 
-#jfk = 40.38
-#jfklong=73.47
-
-#check = dist_between(laxlat, laxlong, jfk,jfklong)
-
 def calculate_distances(lat1, long1):
     # pass the lat and long of the zip code
     # calculate distance to each weather station
     mydistances=[]
-    n=len(WeatherStations)
+    n=len(WeatherStations_df)
     for i in range(n):
-        lat2 = float( WeatherStations.iloc[i].lat)
-        long2=float( WeatherStations.iloc[i].long)
+        lat2 = float( WeatherStations_df.iloc[i].lat)
+        long2=float( WeatherStations_df.iloc[i].long)
         mydistances.append(dist_between(lat1, long1, lat2, long2))
     return mydistances
 
-def attach_distances(thedistances):
-    WeatherStations['distance']=thedistances
+def attach_distances(lat, long):
+    thedistances = calculate_distances(lat,long)
+    WeatherStations_df['distance']=thedistances
 
 def sort_get_min_dist_WeatherStat():
-    # sort the weather stations by distance
-    # cast the type of end
-    WeatherStations['end']=WeatherStations['end'].astype('int')
-    datefilter = WeatherStations['end']>=2022
-    FilteredStations = WeatherStations[datefilter]
-    WeatherStations = WeatherStations.sort_values(by=['distance'])
-    return WeatherStations[0:10]
+    """  sort the weather stations by TMAX and distance
+    :return:
+    """
+    WeatherStations_df['end']=WeatherStations_df['end'].astype('int')
+    datefilter = WeatherStations_df['end']>=2022
+    FilteredStations = WeatherStations_df[datefilter]
+    FilteredStations = FilteredStations.sort_values(by=['distance'])
+    return FilteredStations[0:10]
     # find the nearest weather station by year
 
