53b281f89ec64d9f89f651359a92a6e345d4d4c7
heatheranndye <32517963+heatheranndye@users.noreply.github.com>
2022-11-10 09:44:44 -0600

    Changes based on review from 9/11/2022

--- test_colors.py
+++ test_colors.py
@@ -10,18 +10,14 @@ def test_color_conversion():
     assert type(color_conversion_rgb([1, 0, 0, 0])) == RGBColor
 
 
-def test_color_conversion_red():
-    assert color_conversion_rgb([0, 1, 1, 0]) == RGBColor(255, 0, 0)
-
-
-def test_color_conversion_magenta():
-    assert color_conversion_rgb([0, 1, 0, 0]) == RGBColor(255, 0, 255)
-
-
-# TODO: Use table https://www.rapidtables.com/convert/color/cmyk-to-rgb.html
 @pytest.mark.parametrize(
     "cmyk, expected",
-    [([0, 0, 0, 1], RGBColor(0, 0, 0)), ([1, 1, 0, 0], RGBColor(0, 0, 255))],
+    [
+        ([0, 1, 0, 0], RGBColor(255, 0, 255)),
+        ([0, 1, 1, 0], RGBColor(255, 0, 0)),
+        ([0, 0, 0, 1], RGBColor(0, 0, 0)),
+        ([1, 1, 0, 0], RGBColor(0, 0, 255)),
+    ],
 )
 def test_color_conversion_mass(cmyk, expected):
     assert color_conversion_rgb(cmyk) == expected
@@ -40,4 +36,15 @@ def test_string_rep(color_information):
     assert str(color_information) == "Lilac(RGBColor(R=255, G=0, B=255),14)"
 
 
-my_dict = make_kona_dictionary()
+@pytest.fixture
+def color_dictionary():
+    return make_kona_dictionary()
+
+
+def test_dictionary_type(color_dictionary):
+    assert isinstance(color_dictionary, dict)
+
+def test_dictionary_entry(color_dictionary):
+    assert str(color_dictionary.get(1)=="ColorInformation('Turquoise','RGBColor(R=58, G=193, B=225)','1') ")
+
+
