e63dca4accbac6dcd7a299ea639899e2e41415cf
heatheranndye <32517963+heatheranndye@users.noreply.github.com>
2022-12-02 09:16:12 -0600

    Fix package structure

--- dataquilt/dataquilt/__init__.py
+++
@@ -1,3 +0,0 @@
-import pathlib
-
-DATA_PATH = pathlib.Path(__file__).parent.parent.parent / "data"
--- dataquilt/setup.py
+++ dataquilt/setup.py
@@ -3,10 +3,14 @@ from setuptools import find_packages, setup
 # Add install requirements
 setup(
     author="Heather",
-    description="""A package that allows to generate dataquilt visualizations based on weather data.""",
+    description="""A package to generate dataquilt visualizations\\
+    based on weather data.""",
     name="dataquilt",
-    packages=find_packages(include=["dataquilt"]),
-    version="0.1.0",
-    install_requires=["numpy>=1.18.0", "numpy-financial>=1.0.0", "pandas"],
-    python_requires=">=3.6",
+    version="0.0.1",
+    packages=find_packages(where="src", include=["dataquilt", "data"]),
+    package_dir={"": "src"},
+    package_data={"data": ["*.txt", "*.csv", ".ase"]},
+    # version="0.1.0",
+    # install_requires=["numpy>=1.18.0", "numpy-financial>=1.0.0", "pandas"],
+    # python_requires=">=3.6",
 )
