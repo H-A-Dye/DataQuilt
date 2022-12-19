7c731f303a6b83568dd2680be692b075c0d9f5b2
heatheranndye <32517963+heatheranndye@users.noreply.github.com>
2022-12-19 11:48:41 -0600

    Fix streamlit app

--- stream/streamlit_app.py
+++ stream/streamlit_app.py
@@ -25,7 +25,7 @@ st.set_page_config(
 
 st.title("Temperature Quilt")
 st.markdown(
-    """##### Construct a temperature quilt for a US zip code
+    """##### Construct a temperature quilt for a US zip code and year
             """,
     unsafe_allow_html=True,
 )
@@ -40,18 +40,21 @@ with col3:
 
 st.sidebar.markdown(" ## Temperature Quilts")
 
-random_string = (
-    "This is a veeeeeeeeeeeeeeeeeeeeeeeeeery looooong striiiing. "
-    "And this is anooooooooother veeeeeeery loooooooong striiiing"
+
+prog_intro = (
+    "This program makes a model of a year of "
+    "maximum daily temperatures. The data is "
+    "obtained from NOAA based on zip code and year."
 )
 
+st.sidebar.write(prog_intro)
 
-st.sidebar.markdown("This program makes a model of a year of temperatures.")
+
 st.sidebar.info(
     "More about quilting at [www.heatheranndye.com](www.heatheranndye.com)",
     icon="ℹ️",
 )
-st.markdown(random_string)
+
 zip_code = st.text_input("Enter a US zipcode", value="62269", max_chars=5)
 year = st.number_input("Enter a previous year", value=2021, max_value=2021)
 invlist = dw.load_weatherstation_inventory()
@@ -80,8 +83,8 @@ st.markdown(center_row_text, unsafe_allow_html=True)
 
 inv_df = dw.attach_distances_to_inventory(loc_lat, loc_long, inv_df)
 shortlist = dw.sort_get_min_dist_weatherstat(inv_df)
+st.write("### Ten Nearest Weather Stations to Zip Code")
 
-st.write("### The Ten closest Weather Stations")
 st.dataframe(data=shortlist)
 
 output = da.find_complete_data(shortlist, str(year))
@@ -92,29 +95,38 @@ weather_data_df, num_dates_missing = output
 if num_dates_missing > 0:
     raise ValueError("Missing Dates")
 
+station_name = weather_data_df.STATION[0]
 
+st.markdown("#### Data from station:")
+st.write(station_name)
+explain_nums = "To convert the data to Celsius, divide the data by 10."
+
+st.write(explain_nums)
 st.dataframe(data=weather_data_df)
 
 level_df = ig.create_temp_level_df(weather_data_df)
 
 
-local_im = ig.construct_image_v2(level_df)
+local_image = ig.construct_image_v2(level_df)
 
+st.write("### Sample Quilt Image")
+st.image(local_image)
 
-st.image(local_im)
-
-local_im.save("localpic.jpg")
+local_image.save("localpic.jpg")
 
 st.write("### Color codes for fabric squares")
+
 
 st.dataframe(level_df)
 st.write("### Total squares by color")
 piece_df = ig.create_piece_counter(level_df)
 
+bins = ig.create_bin_list(weather_data_df)
+
 
 st.dataframe(piece_df)
 
-borb_pattern(local_im, piece_df, level_df)
+borb_pattern(local_image, piece_df, level_df)
 
 the_data = open("output.pdf", "rb")
 
